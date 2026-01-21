4900555f4715450116f309568239f8bbcc1a8a65
NUTTOD <nuttod88@gmail.com>
2026-01-21 16:14:14 +0700

    p09_3

--- src/practicals/p06/unique.ts
+++ src/practicals/p06/unique.ts
@@ -1,6 +1,8 @@
 function getUniqueNumbers(arr1: number[], arr2: number[]): number[] {
-  // Write your code below
+  const merged = [...arr1, ...arr2]
+  return merged
 }
 
 const arr1 = [1, 2, 3, 4];
 const arr2 = [3, 4, 5, 6];
+console.log(getUniqueNumbers(arr1, arr2))
--- src/practicals/p09/find-data.ts
+++ src/practicals/p09/find-data.ts
@@ -1,3 +1,7 @@
+import axios from "axios";
+import users from "./datasets/users.json"
+import products from "./datasets/products.json"
+
 function findData(fileName: string, key: string, value: string | number): void {
-  
+    
 }
