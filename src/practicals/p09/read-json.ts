44b79107e54b9faebbb9bdb0699c70a07c2358c9
NUTTOD <nuttod88@gmail.com>
2026-01-21 15:23:05 +0700

    p09_1

--- src/practicals/p09/read-json.ts
+++ src/practicals/p09/read-json.ts
@@ -1,3 +1,22 @@
+import users from "./datasets/users.json"
+import products from "./datasets/products.json"
+
 function readJson(fileName: string): unknown[] {
-  
+    const data = JSON.parse(fileName)
+    try{
+        if (!data) {
+            const err = new Error("Invalid JSON format")
+            err.name = "InvalidFormat"
+            throw err
+        }
+    } catch (e:any) {
+        if (e.name === "InvalidFormat") {
+            console.error(e.message)
+        }
+        console.error(e.message)
+    }
+    return data
 }
+
+readJson(JSON.stringify(users))
+readJson(JSON.stringify(products))
