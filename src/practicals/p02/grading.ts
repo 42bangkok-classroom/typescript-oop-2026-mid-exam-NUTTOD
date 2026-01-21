8293b08e6e803bc7b16b63d994d6d2c60a2b58f5
NUTTOD <nuttod88@gmail.com>
2026-01-21 14:34:06 +0700

    p02_1

--- src/practicals/p02/grading.ts
+++ src/practicals/p02/grading.ts
@@ -1,6 +1,26 @@
 const input = +process.argv[2]
 function grade(input: number) {
-    if (isNaN(input)){
-        return
+    if (isNaN(input) || input > 100 || input < 0){
+        console.log("Invalid input")
+    }
+    else if (input >= 80 && input <= 100){
+        console.log("Grade is A")
+    }
+    else if (input >= 70 && input <= 79){
+        console.log("Grade is B")
+    }
+    else if (input >= 60 && input <= 69){
+        console.log("Grade is C")
+    }
+    else if (input >= 50 && input <= 59){
+        console.log("Grade is D")
+    }
+    else if (input >= 0 && input <= 49){
+        console.log("Grade is F")
+    }
+    else {
+        console.log("Invalid input")
     }
 }
+
+grade(input)
