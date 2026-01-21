c0d9687113b56f967a27a7fe14d495badf8f472b
NUTTOD <nuttod88@gmail.com>
2026-01-21 16:25:33 +0700

    p03_7

--- src/practicals/p03/fizzbuzz.ts
+++ src/practicals/p03/fizzbuzz.ts
@@ -1,7 +1,7 @@
 const input_03 = +process.argv[2]
 
 let answer: string[] = [];
- for (let i = 0; i <= input_03; i++) {
+ for (let i = 1; i <= input_03; i++) {
      if (i % 3 === 0 && i % 5 === 0) {
          console.log("FizzBuzz");
      } else if (i % 3 === 0) {
