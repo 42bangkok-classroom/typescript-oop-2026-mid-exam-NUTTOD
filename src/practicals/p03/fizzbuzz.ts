a0f2798cbdf8f35ac93687b9042e987f0cf3dd27
NUTTOD <nuttod88@gmail.com>
2026-01-21 14:44:14 +0700

    p03_1

--- src/practicals/p03/fizzbuzz.ts
+++ src/practicals/p03/fizzbuzz.ts
@@ -1,1 +1,16 @@
-// Write your code below
+const input_03 = +process.argv[2]
+
+for (let i = 1; i <= input_03; i++){
+    if (i % 5 === 0){
+        console.log("Buzz")
+    }
+    else if (i % 3 === 0){
+        console.log("Fizz")
+    }
+    else if (i % 3 === 0 && input_03 % 5 === 0){
+        console.log("FizzBuzz")
+    }
+    else {
+        console.log(i)
+    }
+}
